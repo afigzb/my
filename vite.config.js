21bd5dc7d31c16a8afec611af1814973a16fd2b2
aliyun0056826799-Pa6Fg <361747532@qq.com>
2025-05-30 19:36:44 +0800

    修改配置

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,31 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { viteStaticCopy } from 'vite-plugin-static-copy'
+import { resolve } from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    viteStaticCopy({
+      targets: [
+        {
+          src: 'src/OldComponents/**/*',
+          dest: 'src/OldComponents'
+        }
+      ]
+    })
+  ],
   base: './',
+  resolve: {
+    alias: {
+      '@': resolve(__dirname, 'src'),
+    }
+  },
+  server: {
+    fs: {
+      // 允许访问 src 目录及其子目录
+      allow: ['..']
+    }
+  }
 })
